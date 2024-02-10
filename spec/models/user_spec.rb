53001245e197325e5fb69fe2ad9c5181c24ae269
koyuzu0310 <akabane1225t@gmail.com>
2024-02-10 21:09:00 +0900

    ユーザー管理機能実装

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -40,19 +40,6 @@ RSpec.describe User, type: :model do
       expect(@user.errors.full_messages).to include("Password confirmation doesn't match Password")
     end
 
-    it 'nicknameが7文字以上では登録できない' do
-      @user.nickname = 'aaaaaaa'
-      @user.valid?
-      expect(@user.errors.full_messages).to include('Nickname is too long (maximum is 6 characters)')
-    end
-
-    it '重複したemailが存在する場合は登録できない' do
-      @user.save
-      another_user = FactoryBot.build(:user)
-      another_user.email = @user.email
-      another_user.valid?
-      expect(another_user.errors.full_messages).to include('Email has already been taken')
-    end
 
     it 'emailは@を含まないと登録できない' do
       @user.email = 'testmail'
