f1b9b4182b301e7484b9aa3575299d2571980e85
koyuzu0310 <akabane1225t@gmail.com>
2024-02-11 21:39:12 +0900

    ユーザー管理機能修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,6 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
          validates :nickname, presence: true, length: { minimum: 6 }
-         #validates :password, format: { with: /\A(?=.*?[a-zA-Z])(?=.*?\d)[a-zA-Z\d]+\z/}
          validates :password, presence: true, format: { with: /\A(?=.*?[a-zA-Z])(?=.*?\d)[a-zA-Z\d]+\z/}
          validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥ヴァーヶ々]+\z/}
          validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥ヴァーヶ々]+\z/}
