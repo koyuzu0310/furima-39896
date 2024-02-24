5b5b050c7e47f3193058527e99447d81d2c6a8c6
koyuzu0310 <akabane1225t@gmail.com>
2024-02-24 20:27:29 +0900

    商品情報編集機能実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,19 +1,28 @@
 class ItemsController < ApplicationController
 
- before_action :authenticate_user!, except: [:index, :show]
+ before_action :authenticate_user!, except: [:index, :show,]
+ before_action :move_to_index, except: [:index, :show]
 
+ def move_to_index
+  unless user_signed_in?
+    redirect_to action: :index
+  end
+end
+ 
  def edit
   @item = Item.find(params[:id])
 end
 
 
- def update
-  item = Item.find(params[:id])
-  item.update(item_params)
-  redirect_to root_path
+def update
+  @item = Item.find(params[:id])
+  if @item.update(item_params)
+    redirect_to root_path, notice: "アイテムが更新されました"
+  else
+    render :edit, status: :unprocessable_entity
+  end
 end
 
- 
 
  def show
   @item = Item.find(params[:id])
@@ -40,7 +49,7 @@ end
   def item_params
     params.require(:item).permit(:image, :item_name, :item_info, :item_category_id, :item_sales_status_id,
       :item_shipping_fee_status_id, :item_prefecture_id, :item_scheduled_delivery_id, :item_price).merge(user_id: current_user.id)
-  end
-
-
 end
+end
+
+
