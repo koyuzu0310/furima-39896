c443c84e674a7d6fb6c7e2cffe1a6fc08e193464
koyuzu0310 <akabane1225t@gmail.com>
2024-02-25 00:59:23 +0900

    商品情報編集機能修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,55 +1,60 @@
 class ItemsController < ApplicationController
-
+  before_action :set_item, only: [:edit, :show]
  before_action :authenticate_user!, except: [:index, :show,]
- before_action :move_to_index, except: [:index, :show]
+ before_action :contributor_confirmation, only: [:edit, :update]
 
- def move_to_index
-  unless user_signed_in?
-    redirect_to action: :index
+ def index
+  @items = Item.order(created_at: :desc)
+end
+
+def new
+  @item = Item.new
+end
+
+def show
+  @item = Item.find(params[:id])
+ end
+
+ def create
+  @item = Item.new(item_params)
+  if @item.save
+    redirect_to item_path
+  else
+    render :new, status: :unprocessable_entity
   end
 end
- 
- def edit
-  @item = Item.find(params[:id])
-end
 
+def edit
+  if current_user != @item.user
+    redirect_to root_path
+  end
+end
 
 def update
   @item = Item.find(params[:id])
   if @item.update(item_params)
-    redirect_to root_path, notice: "アイテムが更新されました"
+    redirect_to item_path
   else
     render :edit, status: :unprocessable_entity
   end
 end
 
 
- def show
-  @item = Item.find(params[:id])
- end
-
- def index
-  @items = Item.order(created_at: :desc)
-end
-  def new
-    @item = Item.new
-  end
-
-  def create
-    @item = Item.new(item_params)
-    if @item.save
-      redirect_to root_path
-    else
-      render :new, status: :unprocessable_entity
-
-    end
-  end
-
-  private
+private
   def item_params
     params.require(:item).permit(:image, :item_name, :item_info, :item_category_id, :item_sales_status_id,
       :item_shipping_fee_status_id, :item_prefecture_id, :item_scheduled_delivery_id, :item_price).merge(user_id: current_user.id)
 end
+
+ def set_item
+  @item = Item.find(params[:id])
+end
+
+def contributor_confirmation
+  redirect_to action: :index unless current_user == @item&.user
+end
+
+
 end
 
 
