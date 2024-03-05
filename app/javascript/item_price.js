367dab1a9c41383d1fb1c5677153d7de2402503d
koyuzu0310 <akabane1225t@gmail.com>
2024-03-05 17:14:03 +0900

    修正

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -1,4 +1,4 @@
-window.addEventListener('turbo:load', () => {
+const price = () => {
   const priceInput = document.getElementById("item-price");
 
   priceInput.addEventListener('input', () => {
@@ -16,24 +16,7 @@ window.addEventListener('turbo:load', () => {
       document.getElementById("profit").textContent = 0;
     }
   });
+};
 
-  // turbo:render イベントのハンドラを追加する
-  window.addEventListener("turbo:render", pay);
-});
-
-function pay() {
-  const priceInput = document.getElementById("item-price");
-  const price = parseFloat(priceInput.value);
-
-  if (!isNaN(price)) {
-    const taxPrice = Math.floor(price * 0.1);
-    const profit = Math.floor(price * 0.9);
-
-    document.getElementById("add-tax-price").textContent = taxPrice;
-    document.getElementById("profit").textContent = profit;
-  } else {
-    // 金額が数値ではない場合の処理
-    document.getElementById("add-tax-price").textContent = 0;
-    document.getElementById("profit").textContent = 0;
-  }
-}
+window.addEventListener("turbo:load",price);
+window.addEventListener("turbo:render", price)
