43684d60393e63428073c84da6aba6773cd95722
koyuzu0310 <akabane1225t@gmail.com>
2024-03-05 12:15:15 +0900

    該当箇所修正

--- app/models/item_scheduled_delivery.rb
+++ app/models/item_scheduled_delivery.rb
@@ -1,9 +1,9 @@
 class ItemScheduledDelivery < ActiveHash::Base
   self.data = [
     { id: 1, name: '---' },
-    { id: 2, name: '１～２で発送' },
-    { id: 3, name: '２～３で発送' },
-    { id: 4, name: '４～７で発送' },
+    { id: 2, name: '１～２日で発送' },
+    { id: 3, name: '２～３日で発送' },
+    { id: 4, name: '４～７日で発送' },
    
   ]
 
--- app/models/order_form.rb
+++ app/models/order_form.rb
@@ -8,7 +8,7 @@ class OrderForm
     validates :user_id
     validates :token
     validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
-    validates :item_prefecture_id, numericality: {other_than: 0, message: "can't be blank"}
+    validates :item_prefecture_id, presence: true, numericality: { other_than: 1 }
     validates :city
     validates :address
     validates :phone_number, format: { with: /\A\d{10,11}\z/,message: "is invalid. Include hyphen(-)"}
